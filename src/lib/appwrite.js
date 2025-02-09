f84aab28bcfc9979dde9c010a8c6d44e1be5e263
YT <accmanager1990@gmail.com>
2025-02-09 14:37:37 +0900

    t

--- src/lib/appwrite.js
+++ src/lib/appwrite.js
@@ -1,10 +1,9 @@
 import { dev } from "$app/environment";
 import { Client, Databases, Account } from "appwrite";
-import { env } from "$env/dynamic/private";
 const client = new Client();
 
-const EP = dev ? 'http://localhost/v1' : env.ENDPOINT
-const P_ID = dev ? '67a80ed90022063b7089' : env.PROJECT_ID;
+const EP = dev ? 'http://localhost/v1' : "https://cloud.appwrite.io/v1"
+const P_ID = dev ? '67a80ed90022063b7089' : "67a7686f0026476fde72"
 client.setEndpoint(EP).setProject(P_ID)
 
 export const account = new Account(client)
