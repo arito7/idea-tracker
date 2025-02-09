fb593eee6a6948b6ac4b9e61c2c9c34d6e8ccabf
YT <accmanager1990@gmail.com>
2025-02-09 14:21:04 +0900

    test gh actions

--- src/lib/appwrite.js
+++ src/lib/appwrite.js
@@ -1,11 +1,11 @@
 import { dev } from "$app/environment";
 import { Client, Databases, Account } from "appwrite";
-
+import { env } from "$env/dynamic/private";
 const client = new Client();
 
-const ENDPOINT = dev ? 'http://localhost/v1' : 'https://cloud.appwrite.io/v1'
-const PROJECT_ID = dev ? '67a80ed90022063b7089' : '67a7686f0026476fde72';
-client.setEndpoint(ENDPOINT).setProject(PROJECT_ID)
+const EP = dev ? 'http://localhost/v1' : env.ENDPOINT
+const P_ID = dev ? '67a80ed90022063b7089' : env.PROJECT_ID;
+client.setEndpoint(EP).setProject(P_ID)
 
 export const account = new Account(client)
 export const databases = new Databases(client)
